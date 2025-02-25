901575ab61eb14d353340e4e6773dc52c3e6b32c
loqusion <loqusion@gmail.com>
2025-02-25 06:03:01 -0600

    Simpler repro

--- repro.rs
+++ repro.rs
@@ -1,30 +1,17 @@
-use std::time::Duration;
-
-use tracing::Level;
+use tracing::{Level, Subscriber};
 use tracing_mock::{expect, subscriber};
 
 #[tokio::test]
 #[should_panic]
 async fn repro_should_fail() {
-    let (subscriber, handle) = subscriber::mock()
-        .event(
-            expect::event()
-                .at_level(Level::ERROR)
-                .with_fields(expect::field("message").with_value(&"not real message")),
-        )
-        .run_with_handle();
-
-    const SLEEP_TIME: Duration = Duration::from_millis(40);
+    let (subscriber, handle) = subscriber_mock();
 
     {
         let _guard = tracing::subscriber::set_default(subscriber);
-
         tokio::spawn(async {
-            tokio::time::sleep(SLEEP_TIME).await;
             tracing::error!("real message");
         });
-
-        tokio::time::sleep(SLEEP_TIME + Duration::from_millis(10)).await;
+        tokio::task::yield_now().await;
     }
 
     handle.assert_finished();
@@ -33,13 +20,7 @@ async fn repro_should_fail() {
 #[tokio::test]
 #[should_panic = "[fails_as_expected]"]
 async fn fails_as_expected() {
-    let (subscriber, handle) = subscriber::mock()
-        .event(
-            expect::event()
-                .at_level(Level::ERROR)
-                .with_fields(expect::field("message").with_value(&"not real message")),
-        )
-        .run_with_handle();
+    let (subscriber, handle) = subscriber_mock();
 
     {
         let _guard = tracing::subscriber::set_default(subscriber);
@@ -47,4 +28,14 @@ async fn fails_as_expected() {
     }
 
     handle.assert_finished();
+}
+
+fn subscriber_mock() -> (impl Subscriber, subscriber::MockHandle) {
+    subscriber::mock()
+        .event(
+            expect::event()
+                .at_level(Level::ERROR)
+                .with_fields(expect::field("message").with_value(&"not real message")),
+        )
+        .run_with_handle()
 }
