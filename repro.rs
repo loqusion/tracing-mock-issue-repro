a95d0ea21d4cc8776740f300bfde379f945ca6db
loqusion <loqusion@gmail.com>
2025-02-25 06:13:16 -0600

    Remove `at_level`

--- repro.rs
+++ repro.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-use tracing::{Level, Subscriber};
+use tracing::Subscriber;
 use tracing_mock::{expect, subscriber};
 
 #[tokio::test]
@@ -36,7 +36,6 @@ fn subscriber_mock() -> (impl Subscriber, subscriber::MockHandle) {
     subscriber::mock()
         .event(
             expect::event()
-                .at_level(Level::ERROR)
                 .with_fields(expect::field("message").with_value(&debug_value("not real message"))),
         )
         .run_with_handle()
